af2244dad1f1e25d0c81313150fc8afe5eaab282
Rahul Varughese <rahulvarughese02@gmail.com>
2024-08-18 22:09:00 -0500

    edit connections.js

--- config/connections.js
+++ config/connections.js
@@ -1,23 +1,15 @@
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres'
-    },
-  );
+const { Sequelize } = require('sequelize');
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    },
-  );
-}
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+      }
+    );
+
+module.exports = sequelize;
